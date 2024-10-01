79a4dc30874d0e2b4ff548556ce82020a7e0491b
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2024-10-01 16:30:50 -0400

    randy cleaned up halle's amazingnessess.  see randys_code.  we can/will discuss.

--- code/randys_code.R
+++ code/randys_code.R
@@ -1,11 +1,26 @@
 ## read in and clean burbot assessment larva data
 
-# load dependancies
+# load dependencies
 library(tidyverse)
+# stringr and tidyr are a part of tidyverse so you only need to load tidyverse
 
 
 raw_data <- read.csv('data/Burbot_Larval Assessment.csv')
 
 clean_data <- raw_data %>%
   group_by(REPRESENTATIVE_TRS) %>%
-  summarize(count = sum(FREQUENCY))
+  summarize(count = sum(FREQUENCY)) %>%
+  separate(REPRESENTATIVE_TRS, 
+           into = c("Township", "Range", "Section", "Section Location"), 
+           sep = "/") %>%
+  mutate(Section = as.numeric(Section)) %>%
+  mutate(Section = str_pad(Section, width = 2, pad = "0")) %>%
+  na.omit() %>%
+  mutate(TWNRNSEC = paste0(Township, Range, Section))
+
+
+
+# Export the entire data frame to a CSV file
+write.csv(SuperCleanData, "data/SuperCleanBurbotData", row.names = FALSE)
+
+
