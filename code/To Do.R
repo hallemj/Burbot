72b0c6e4a548eddba151c77cdbf34c691c17b913
hallemj <hallemj1@gmail.com>
2024-10-03 11:28:35 -0400

    changes i made also added stiff to to do list

---
+++ code/To Do.R
@@ -0,0 +1,11 @@
+## create a google drive with all of our Lota lota things :p
+
+# 1: Explore the clean_data
+#   - explore patterns of the count (what is the distribution of the count?)
+#   - to find nice intervals (average count? range?)
+#   - use R Graph Gallery to generate meaningful charts based on the count field
+# goal - understand our count data 
+# task - make histogram of count field 
+#      - remove all the rows with zero count
+#      - remake histogram
+#      - look into making choropleth map (polygons based on a value - count) from set breaks ()
